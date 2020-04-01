8e5c4a05cf14433cb608d27cb4f2e0f490cb82ed
Elisa <elisa@neopath.com.br>
2020-04-01 11:39:59 -0300

    reconfiguracao do progama

--- findAge.test.js
+++ findAge.test.js
@@ -1,8 +1,8 @@
 const findAge = require('./findAge');
 
 test('parse phrase', () => {
-    findAge.Parsing('abc def tem 20 anos de idade');
-    expect(findAge.list[0]).toEqual(['abc def', '20']);
+    findAge.Parsing('abc tem 20 anos de idade');
+    expect(findAge.list[0]).toEqual(['abc', '20']);
 });
 
 test('return 20', () => {
