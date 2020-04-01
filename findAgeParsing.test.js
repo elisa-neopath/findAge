e6c52a8f5a871cda01c68e630af81046d887b28a
Elisa <elisa@neopath.com.br>
2020-04-01 12:43:00 -0300

    setup e teardown

--- findAgeParsing.test.js
+++ findAgeParsing.test.js
@@ -1,6 +1,16 @@
 const findAge = require('./findAge');
 
+afterAll(() => {
+    findAge.list = [];
+    findAge.counter = 0;
+})
+
 test('parse phrase', () => {
-    findAge.Parsing('abc tem 20 anos de idade');
-    expect(findAge.list[0]).toEqual(['abc', '20']);
+    findAge.Parsing('joao tem 20 anos de idade');
+    expect(findAge.list[0]).toEqual(['joao', '20']);
+});
+
+test('parse phrase', () => {
+    findAge.Parsing('maria tem 30 anos de idade');
+    expect(findAge.list[1]).toEqual(['maria', '30']);
 });
--- findAgeResponde.test.js
+++ findAgeResponde.test.js
@@ -1,9 +1,28 @@
 const findAge = require('./findAge');
 
+beforeAll(() => {
+    findAge.Parsing('abc tem 20 anos de idade');
+    findAge.Parsing('def tem 10 anos de idade');
+    findAge.Parsing('ghi tem 30 anos de idade');
+});
+
+afterAll(() => {
+    findAge.list = [];
+    findAge.counter = 0;
+});
+
 test('return 20', () => {
     expect(findAge.Responde('abc')).toBe('20');
 });
 
+test('return 10', () => {
+    expect(findAge.Responde('def')).toBe('10');
+});
+
+test('return 30', () => {
+    expect(findAge.Responde('ghi')).toBe('30');
+});
+
 test('return not found', () => {
-    expect(findAge.Responde('ghi')).toBe('not found');
+    expect(findAge.Responde('jkl')).toBe('not found');
 });
